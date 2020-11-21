4ef564a46a93824b6bd605df4668265f5c89150f
Joe Phillips <joe147258@hotmail.co.uk>
2020-11-21 14:54:27 +0000

    Further work on logins, more robust less errors

--- main.py
+++ main.py
@@ -2,65 +2,70 @@ from flask import Flask, render_template, request, redirect, url_for
 from controller.quiz_controller import quiz_controller
 import flask_login
 from flask_login import LoginManager, UserMixin
+from hashlib import sha256
+from utilities import config
 
+# App set up
 login_manager = LoginManager()
 app = Flask(__name__)
 login_manager.init_app(app)
 app.secret_key = 'key'
 
-
 app.register_blueprint(quiz_controller)
 
-users = {'user1':{'pw':'pass1'}, 
-         'user2':{'pw':'pass2'}, 
-         'user3':{'pw':'pass3'}}
+users = { config.admin_name():{'pw':config.admin_password()} }
 
 class User(UserMixin):
   pass
 
 @login_manager.user_loader
 def user_loader(username):
-  if username not in users:
-    return 0
+    if username not in users:
+        return 0
 
-  user = User()
-  user.id = username
-  return user
+    user = User()
+    user.id = username
+    return user
 
 @login_manager.request_loader
 def request_loader(request):
-  username = request.form.get('username')
-  if username not in users:
-    return
+    username = request.form.get('username')
+    if username not in users:
+        return
 
-  user = User()
-  user.id = username
-
-  user.is_authenticated = request.form['pw'] == users[username]['pw']
-
-  return user
+    user = User()
+    user.id = username
+    try:
+        user.is_authenticated = request.form['pw'] == users[username]['pw']
+    except Exception:
+        return redirect(url_for('login') + '?error=1')
+    
+    return user
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  if request.method == 'POST':
-    username = request.form.get('username')
-    if request.form.get('pw') == users[username]['pw']:
-      user = User()
-      user.id = username
-      flask_login.login_user(user)
-      return redirect(url_for('admin'))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        try:
+            if request.form.get('pw') == users[username]['pw']:
+                user = User()
+                user.id = username
+                flask_login.login_user(user)
+                return redirect(url_for('admin'))
+        except Exception:
+            return redirect(url_for('login') + '?error=1')
 
-  return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/admin')
 @flask_login.login_required
 def admin():
-  return render_template('admin.html')
+    return render_template('admin.html')
 
 @app.route('/logout')
 def logout():
-  flask_login.logout_user()
-  return 'Logged out'
+    flask_login.logout_user()
+    return 'Logged out'
   
 @login_manager.unauthorized_handler
 def unauthorized_callback():
--- utilities/config.py
+++ utilities/config.py
@@ -7,7 +7,7 @@ import json
 def config():
     configFile = open("config.json")
     configData = json.load(configFile)
-    return configData;
+    return configData
 
 def x_movement():
     return config()["x_movement"]
@@ -20,3 +20,14 @@ def image_delete_time():
 
 def intro_text():
     return config()["site_info"]["intro_text"]
+
+def admin_config():
+    configFile = open("admin_user.json")
+    configData = json.load(configFile)
+    return configData
+
+def admin_name():
+    return admin_config()["username"]
+    
+def admin_password():
+    return admin_config()["password"]
